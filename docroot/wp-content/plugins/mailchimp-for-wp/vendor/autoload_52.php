42702c7a2bc2f54568b0f2286e3f4c4cc7b335f7
Hai Cao <hai.cao@ffwagency.com>
2019-11-05 11:45:42 +0700

    Update core and modules

--- docroot/wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
+++ docroot/wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInita19e34f417e2b6f8a0edb8982a25f210::getLoader();
+return ComposerAutoloaderInitb1b56e406c081324ba744ad0d6fb803d::getLoader();
